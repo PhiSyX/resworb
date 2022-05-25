a310d42f777d0705a2c1722a4d1aee62c5c42d81
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-05-25 19:42:29 +0200

    HTML Tree Construction: le mode d'insertion "in select" (#61)
    * feat(html): jeton de caractère NULL #60
    
    * feat(html): jeton de caractère #60
    
    * feat(html): jeton de commentaire #60
    
    * feat(html): jeton doctype #60
    
    * feat(html): jeton balise de début "html" #60
    
    * feat(html): jeton balise de début "option" #60
    
    * feat(html): jeton balise de début "optgroup" #60
    
    * feat(html): jeton balise de fin "optgroup" #60
    
    * feat(html): jeton balise de fin "option" #60
    
    * feat(html): jeton balise de fin "select" #60
    
    * feat(html): jeton balise de début #60
    
    * feat(html): jeton balise #60
    
    * feat(html): jeton EOF #60
    
    * feat(html): jeton anything else #60

--- html/parser/lib.rs
+++ html/parser/lib.rs
@@ -261,7 +261,9 @@ where
             | InsertionMode::InCell => {
                 self.handle_in_cell_insertion_mode(token);
             }
-            | InsertionMode::InSelect => todo!(),
+            | InsertionMode::InSelect => {
+                self.handle_in_select_insertion_mode(token);
+            }
             | InsertionMode::InSelectInTable => todo!(),
             | InsertionMode::InTemplate => {
                 self.handle_in_template_insertion_mode(token)
@@ -709,6 +711,14 @@ where
     /// ouverts.
     fn current_node(&self) -> Option<&TreeNode<Node>> {
         self.stack_of_open_elements.current_node()
+    }
+
+    fn before_current_node(&self) -> Option<&TreeNode<Node>> {
+        let size = self.stack_of_open_elements.len();
+        if size == 0 || size == 1 {
+            return None;
+        }
+        self.stack_of_open_elements.get(size - 2)
     }
 
     /// L'endroit approprié pour insérer un nœud, en utilisant
@@ -5597,7 +5607,7 @@ where
             //
             // Si la pile d'éléments ouverts ne comporte pas d'élément td
             // ou th dans la portée de la table, il s'agit d'une erreur
-            // d'analyse ; ignorez le jeton. (cas d'un fragment)
+            // d'analyse ; ignorer le jeton. (cas d'un fragment)
             // Sinon, nous devons fermer la cellule puis retraiter le
             // jeton.
             | HTMLToken::Tag(HTMLTagToken {
@@ -5702,6 +5712,285 @@ where
                     InsertionMode::InBody,
                     token,
                 );
+            }
+        }
+    }
+
+    fn handle_in_select_insertion_mode(&mut self, token: HTMLToken) {
+        match token {
+            // A character token that is U+0000 NULL
+            //
+            // Erreur d'analyse. Ignorer le jeton.
+            | HTMLToken::Character('\0') => {
+                self.parse_error(&token);
+                /* Ignore */
+            }
+
+            // Any other character token
+            //
+            // Insérer le caractère du jeton.
+            | HTMLToken::Character(ch) => {
+                self.insert_character(ch);
+            }
+
+            // A comment token
+            //
+            // Insérer un commentaire.
+            | HTMLToken::Comment(comment) => {
+                self.insert_comment(comment);
+            }
+
+            // A DOCTYPE token
+            //
+            // Erreur d'analyse. Ignorer le jeton.
+            | HTMLToken::DOCTYPE(_) => {
+                self.parse_error(&token);
+                /* Ignore */
+            }
+
+            // A start tag whose tag name is "html"
+            //
+            // Traiter le jeton en utilisant les règles du mode d'insertion
+            // "in body".
+            | HTMLToken::Tag(HTMLTagToken {
+                ref name,
+                is_end: false,
+                ..
+            }) if tag_names::html == name => {
+                self.process_using_the_rules_for(
+                    InsertionMode::InBody,
+                    token,
+                );
+            }
+
+            // A start tag whose tag name is "option"
+            //
+            // Si le noeud actuel est un élément d'option, il faut retirer
+            // ce noeud de la pile des éléments ouverts.
+            // Insérer un élément HTML pour le jeton.
+            | HTMLToken::Tag(
+                ref tag_token @ HTMLTagToken {
+                    ref name,
+                    is_end: false,
+                    ..
+                },
+            ) if tag_names::option == name => {
+                if let Some(cnode) = self.current_node() {
+                    if cnode.element_ref().tag_name() == tag_names::option
+                    {
+                        self.stack_of_open_elements.pop();
+                    }
+                }
+
+                self.insert_html_element(tag_token);
+            }
+
+            // A start tag whose tag name is "optgroup"
+            //
+            // Si le noeud actuel est un élément option, il faut retirer
+            // ce noeud de la pile des éléments ouverts.
+            // Si le noeud actuel est un élément optgroup, il faut
+            // retirer ce noeud de la pile des éléments ouverts.
+            // Insérer un élément HTML pour le jeton.
+            | HTMLToken::Tag(
+                ref tag_token @ HTMLTagToken {
+                    ref name,
+                    is_end: false,
+                    ..
+                },
+            ) if tag_names::optgroup == name => {
+                if let Some(cnode) = self.current_node() {
+                    if cnode.element_ref().tag_name() == tag_names::option
+                    {
+                        self.stack_of_open_elements.pop();
+                    }
+                }
+
+                if let Some(cnode) = self.current_node() {
+                    if cnode.element_ref().tag_name()
+                        == tag_names::optgroup
+                    {
+                        self.stack_of_open_elements.pop();
+                    }
+                }
+
+                self.insert_html_element(tag_token);
+            }
+
+            // An end tag whose tag name is "optgroup"
+            //
+            // Tout d'abord, si le nœud actuel est un élément d'option et
+            // que le nœud qui le précède immédiatement dans la pile des
+            // éléments ouverts est un élément de groupe d'options, il faut
+            // sortir le nœud actuel de la pile des éléments ouverts.
+            // Si le noeud actuel est un élément d'optgroup, alors il faut
+            // sortir ce noeud de la pile des éléments ouverts. Sinon, il
+            // s'agit d'une erreur d'analyse ; ignorer le jeton.
+            | HTMLToken::Tag(HTMLTagToken {
+                ref name,
+                is_end: true,
+                ..
+            }) if tag_names::optgroup == name => {
+                if let (Some(pnode), Some(cnode)) =
+                    (self.before_current_node(), self.current_node())
+                {
+                    let pelement = pnode.element_ref();
+                    let celement = cnode.element_ref();
+
+                    if celement.tag_name() == tag_names::option
+                        && pelement.tag_name() == tag_names::optgroup
+                    {
+                        self.stack_of_open_elements.pop();
+                    }
+                }
+
+                if let Some(cnode) = self.current_node() {
+                    if cnode.element_ref().tag_name()
+                        == tag_names::optgroup
+                    {
+                        self.stack_of_open_elements.pop();
+                    } else {
+                        self.parse_error(&token);
+                        /* Ignore */
+                    }
+                }
+            }
+
+            // An end tag whose tag name is "option"
+            //
+            // Si le noeud actuel est un élément option, alors il faut
+            // sortir ce noeud de la pile des éléments ouverts. Sinon, il
+            // s'agit d'une erreur d'analyse ; ignorer le jeton.
+            | HTMLToken::Tag(HTMLTagToken {
+                ref name,
+                is_end: true,
+                ..
+            }) if tag_names::option == name => {
+                if let Some(cnode) = self.current_node() {
+                    if cnode.element_ref().tag_name() == tag_names::option
+                    {
+                        self.stack_of_open_elements.pop();
+                    } else {
+                        self.parse_error(&token);
+                        /* Ignore */
+                    }
+                }
+            }
+
+            // An end tag whose tag name is "select"
+            //
+            // Si la pile d'éléments ouverts ne comporte pas d'élément
+            // select dans la portée select, il s'agit d'une erreur
+            // d'analyse ; ignorer le jeton. (cas d'un fragment)
+            // Sinon:
+            // Retirer des éléments de la pile d'éléments ouverts jusqu'à
+            // ce qu'un élément sélectionné ait été retiré de la pile.
+            // Réinitialiser le mode d'insertion de manière appropriée.
+            //
+            // Note: Il est juste traité comme une balise de fin.
+            | HTMLToken::Tag(HTMLTagToken {
+                ref name,
+                is_end: true,
+                ..
+            }) if tag_names::select == name => {
+                if !self
+                    .stack_of_open_elements
+                    .has_element_in_scope_except(
+                        tag_names::select,
+                        StackOfOpenElements::select_scope_elements(),
+                    )
+                {
+                    self.parse_error(&token);
+                    /* Ignore */
+                    return;
+                }
+
+                self.stack_of_open_elements
+                    .pop_until_tag(tag_names::select);
+                self.reset_insertion_mode_appropriately();
+            }
+
+            // A start tag whose tag name is one of: "input", "keygen",
+            // "textarea"
+            //
+            // Erreur d'analyse.
+            // Si la pile d'éléments ouverts ne comporte pas d'élément
+            // select dans la portée select, nous devons ignorer le jeton.
+            // (cas du fragment)
+            // Sinon:
+            // Retirer les éléments de la pile des éléments ouverts jusqu'à
+            // ce qu'un élément sélect ait été sorti de la pile.
+            // Réinitialiser le mode d'insertion de manière appropriée.
+            // Retraiter le jeton.
+            #[allow(deprecated)]
+            | HTMLToken::Tag(HTMLTagToken {
+                ref name,
+                is_end: false,
+                ..
+            }) if name.is_one_of([
+                tag_names::input,
+                tag_names::keygen,
+                tag_names::textarea,
+            ]) =>
+            {
+                if !self
+                    .stack_of_open_elements
+                    .has_element_in_scope_except(
+                        tag_names::select,
+                        StackOfOpenElements::select_scope_elements(),
+                    )
+                {
+                    self.parse_error(&token);
+                    /* Ignore */
+                    return;
+                }
+
+                self.stack_of_open_elements
+                    .pop_until_tag(tag_names::select);
+                self.reset_insertion_mode_appropriately();
+                self.process_using_the_rules_for(
+                    self.insertion_mode,
+                    token,
+                );
+            }
+
+            // A start tag whose tag name is one of: "script", "template"
+            // An end tag whose tag name is "template"
+            //
+            // Traiter le jeton en utilisant les règles du mode d'insertion
+            // "in head".
+            | HTMLToken::Tag(HTMLTagToken {
+                ref name, is_end, ..
+            }) if !is_end
+                && name.is_one_of([
+                    tag_names::script,
+                    tag_names::template,
+                ])
+                || is_end && tag_names::template == name =>
+            {
+                self.process_using_the_rules_for(
+                    InsertionMode::InHead,
+                    token,
+                );
+            }
+
+            // An end-of-file token
+            //
+            // Traite rle jeton en utilisant les règles du mode d'insertion
+            // "in body".
+            | HTMLToken::EOF => {
+                self.process_using_the_rules_for(
+                    InsertionMode::InBody,
+                    token,
+                );
+            }
+
+            // Anything else
+            //
+            // Erreur d'analyse. Ignorer le jeton.
+            | _ => {
+                self.parse_error(&token);
+                /* Ignore */
             }
         }
     }
