0078d1726a7749cef4c7aa0ae0d18d16322ac8c3
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-06-10 16:25:04 +0200

    chore: applique la commande cargo fmt

--- infra/algorithms/parameters.rs
+++ infra/algorithms/parameters.rs
@@ -98,11 +98,10 @@ macro_rules! impl_parameter_type {
 
 impl_parameter_type!(^ *mut T, *const T);
 
-impl_parameter_type!( // Copy
-    usize, u8, u16, u32, u64, u128,
-    isize, i8, i16, i32, i64, i128,
-    f32, f64,
-    bool, char
+impl_parameter_type!(
+    // Copy
+    usize, u8, u16, u32, u64, u128, isize, i8, i16, i32, i64, i128, f32,
+    f64, bool, char
 );
 
 // TODO(phisyx): ajouter une implémentation pour des types génériques.
